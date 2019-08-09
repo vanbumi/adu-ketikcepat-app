c410a1de9b97f548b725649609994e7279b469a0
vanbumi <jsp.dio@gmail.com>
2019-08-09 16:02:46 +0700

    menambahkan game state

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
 import React, { useState } from 'react';
 
 function App() {
+  // 4. create initial game state
+  const INITIAL_GAME_STATE = { victory: false, startTime: null, endTime: null };
+
   // 1. create useState hooks
   const [userText, setUserText] = useState('');
   const [snippet, setSnippet] = useState('');
+
+  const [gameState, setGameState] = useState(INITIAL_GAME_STATE);
 
   // 2. create helper method
   const updateUserText = (event) => {
     setUserText(event.target.value);
     console.log('curent userText', userText);
+
+    if (event.target.value === snippet) {
+      setGameState({
+        ...gameState,
+        victory: true,
+        endTime: new Date().getTime() - gameState.startTime
+      });
+    }
   };
 
   // 3. create SNIPPETS
@@ -21,21 +34,18 @@ function App() {
   const pilihSnippet = snippetIndex => () => {
     console.log('setSnippet', snippetIndex);
     setSnippet(SNIPPETS[snippetIndex]);
+    setGameState({ ...gameState, startTime: new Date().getTime() });
   };
 
   return (
     <div>
       <h2>Adu Ketik Cepat</h2>
-
       <hr />
-
       <h3>Snippet</h3>
       {snippet}
-      <hr />
+      <h4>{gameState.victory ? `HOREE! kecepatan ketik anda: ${gameState.endTime} milidetik` : null}</h4>
       <input value={userText} onChange={updateUserText} />
-
       <hr />
-
       {
         SNIPPETS.map((SNIPPET, index) => (
           <button onClick={pilihSnippet(index)} key={index}>
