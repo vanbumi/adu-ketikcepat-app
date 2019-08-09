daf4856c6164bd8ad6b6382f330a2e139ac62f64
vanbumi <jsp.dio@gmail.com>
2019-08-09 14:01:01 +0700

    menambahkan useState

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function App() {
+  // 1. create useState hooks
+  const [userText, setUserText] = useState('');
+
+  // 2. create helper method
+  const updateUserText = (event) => {
+    setUserText(event.target.value);
+    console.log('curent userText', userText);
+  };
+
   return (
     <div>
       <h2>Adu Ketik Cepat</h2>
-      <input />
+      <input value={userText} onChange={updateUserText} />
     </div>
   );
 }
